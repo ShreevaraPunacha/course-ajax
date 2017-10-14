1a099e2b616b70df5deaf6a3da5bd037ebde574c
Shreevara Punacha <shrivarabhat123@gmail.com>
2017-10-14 16:59:11 +0530

    json accessed and article displayed

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -9,12 +9,15 @@
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
 
+        /** 
         const imageRequest = new XMLHttpRequest();
         imageRequest.onload = addImage;
+        
         imageRequest.onerror = function(err){
             requestError(err,'image');
         };
-        imageRequest.open('GET',`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+         imageRequest.open('GET',`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        
         imageRequest.setRequestHeader('Authorization','Client-ID 1ac41a92a154469b7b9a4c201edb906a7427ab329f21aa8f68dd9777038cfc49');
         imageRequest.send();
         function addImage(){
@@ -33,6 +36,26 @@
                 htmlContent='<div class="error-no-image"> No images available</div>';
             }
             responseContainer.insertAdjacentHTML('afterbegin',htmlContent);
+        }
+        */
+        const articleRequest = new XMLHttpRequest();
+        articleRequest.onload = addArticle;
+        articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=ccc75b773c1b423c99f4bed905338c33`);
+        articleRequest.send();
+        function addArticle(){
+            let htmlContent = '';
+            const data = JSON.parse(this.responseText);
+
+            if(data.response && data.response.docs && data.response.docs.length> 1 ){
+                htmlContent = '<ul>'+ data.response.docs.map(article => `<li class="article">
+               <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+               <p>${article.snippet}</p>
+               </li>`
+                ).join('') + '</ul>';
+            } else{
+                htmlContent = '<div class="error-no-articles">No articles vailable</div>';
+            }
+            responseContainer.insertAdjacentHTML('beforeend',htmlContent);
         }
     });
 })();

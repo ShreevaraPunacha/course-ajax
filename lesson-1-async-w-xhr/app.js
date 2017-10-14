0893d31f48321403f9ba60dfbb06dc3b0279e684
Shreevara Punacha <shrivarabhat123@gmail.com>
2017-10-14 15:44:30 +0530

    data is retrieved from api

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -8,5 +8,18 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        const imageRequest = new XMLHttpRequest();
+        imageRequest.onload = addImage;
+        imageRequest.onerror = function(err){
+            requestError(err,'image');
+        };
+        imageRequest.open('GET',`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        imageRequest.setRequestHeader('Authorization','Client-ID 1ac41a92a154469b7b9a4c201edb906a7427ab329f21aa8f68dd9777038cfc49');
+        imageRequest.send();
+        function addImage(){
+            
+            
+        }
     });
 })();

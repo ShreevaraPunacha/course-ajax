cc6a5b1a32c518ab2920933302339a6e5fd758fe
Shreevara Punacha <shrivarabhat123@gmail.com>
2017-10-14 16:19:15 +0530

    json accessed and image displayed

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -18,8 +18,21 @@
         imageRequest.setRequestHeader('Authorization','Client-ID 1ac41a92a154469b7b9a4c201edb906a7427ab329f21aa8f68dd9777038cfc49');
         imageRequest.send();
         function addImage(){
-            
-            
+
+            let htmlContent = '';
+            const data = JSON.parse(this.responseText);
+
+            if(data && data.results && data.results[0]){
+                const firstImage = data.results[0];
+                htmlContent = `<figure>
+                    <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+                    <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+                    </figure>`;
+            }else
+            {
+                htmlContent='<div class="error-no-image"> No images available</div>';
+            }
+            responseContainer.insertAdjacentHTML('afterbegin',htmlContent);
         }
     });
 })();
